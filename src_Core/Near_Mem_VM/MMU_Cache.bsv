CTSRD-CHERI/Piccolo

commit e5c58a0a14e56d29f2c4385ac9cdf252e8892e34

changed WT caches such that they are compatible with the new consistency approach

src_Core/Near_Mem_VM/MMU_Cache.bsv
@@ -72,6 +72,7 @@ import SourceSink    :: *;
 
 `ifdef PERFORMANCE_MONITORING
 import SpecialRegs :: *;
+import StatCounters :: *;
 `endif
 
 // ================================================================
@@ -162,8 +163,153 @@ interface MMU_Cache_IFC#(numeric type mID);
 `endif
 endinterface
 
-typedef MMU_Cache_IFC#(Wd_MId_2x3) MMU_DCache_IFC;
-typedef MMU_Cache_IFC#(Wd_MId) MMU_ICache_IFC;
+
+interface MMU_DCache_IFC;
+   method Action set_verbosity (Bit #(4) verbosity);
+
+   // Reset request/response
+   interface Server #(Token, Token) server_reset;
+
+   // CPU interface: request
+   (* always_ready *)
+   method Action  req (CacheOp op,
+                       Bit #(3) width_code,
+               Bool is_unsigned,
+`ifdef ISA_A
+                       Bit #(5) amo_funct5,
+`endif
+                       Addr addr,
+               Tuple2#(Bool, Bit#(128)) st_value,
+                       // The following  args for VM
+                       Priv_Mode  priv,
+                       Bit #(1)   sstatus_SUM,
+                       Bit #(1)   mstatus_MXR,
+                       WordXL     satp);    // { VM_Mode, ASID, PPN_for_page_table }
+
+`ifdef ISA_CHERI
+   // CPU interface: commit previous request
+   (* always_ready *)
+   method Action commit;
+`endif
+
+   // CPU interface: response
+   (* always_ready *)  method Bool       valid;
+   (* always_ready *)  method WordXL     addr;        // req addr for which this is a response
+   (* always_ready *)  method Tuple2 #(Bool, CWord) cword; // rd_val data for LD, LR, AMO, SC success/fail result)
+   (* always_ready *)  method Tuple2 #(Bool, CWord) st_amo_val; // Final stored value for ST, SC, AMO
+   (* always_ready *)  method Bool       exc;
+   (* always_ready *)  method Exc_Code   exc_code;
+
+   // Cache flush request/response
+   interface Server #(Token, Token) server_flush;
+
+   // TLB flush
+   method Action tlb_flush;
+
+   // Fabric master interface
+   interface AXI4_Master #( Wd_MId_2x3, Wd_Addr, Wd_Data
+                          , Wd_AW_User, Wd_W_User, Wd_B_User
+                          , Wd_AR_User, Wd_R_User) mem_master;
+
+`ifdef PERFORMANCE_MONITORING
+   method EventsL1D events;
+`endif
+
+   // ----------------------------------------------------------------
+   // Misc. control and status
+
+   // ----------------
+   // For ISA tests: watch memory writes to <tohost> addr
+
+`ifdef WATCH_TOHOST
+   method Action set_watch_tohost (Bool watch_tohost, Bit #(64) tohost_addr);
+   method Bit #(64) mv_tohost_value;
+`endif
+
+   // Inform core that DDR4 has been initialized and is ready to accept requests
+   method Action ma_ddr4_ready;
+
+   // Misc. status; 0 = running, no error
+   (* always_ready *)
+   method Bit #(8) mv_status;
+
+endinterface
+
+interface MMU_ICache_IFC;
+   method Action set_verbosity (Bit #(4) verbosity);
+
+   // Reset request/response
+   interface Server #(Token, Token) server_reset;
+
+   // CPU interface: request
+   (* always_ready *)
+   method Action  req (CacheOp op,
+                       Bit #(3) width_code,
+               Bool is_unsigned,
+`ifdef ISA_A
+                       Bit #(5) amo_funct5,
+`endif
+                       Addr addr,
+               Tuple2#(Bool, Bit#(128)) st_value,
+                       // The following  args for VM
+                       Priv_Mode  priv,
+                       Bit #(1)   sstatus_SUM,
+                       Bit #(1)   mstatus_MXR,
+                       WordXL     satp);    // { VM_Mode, ASID, PPN_for_page_table }
+
+`ifdef ISA_CHERI
+   // CPU interface: commit previous request
+   (* always_ready *)
+   method Action commit;
+`endif
+
+   // CPU interface: response
+   (* always_ready *)  method Bool       valid;
+   (* always_ready *)  method WordXL     addr;        // req addr for which this is a response
+   (* always_ready *)  method Tuple2 #(Bool, CWord) cword; // rd_val data for LD, LR, AMO, SC success/fail result)
+   (* always_ready *)  method Tuple2 #(Bool, CWord) st_amo_val; // Final stored value for ST, SC, AMO
+   (* always_ready *)  method Bool       exc;
+   (* always_ready *)  method Exc_Code   exc_code;
+
+   // Cache flush request/response
+   interface Server #(Token, Token) server_flush;
+
+   // TLB flush
+   method Action tlb_flush;
+
+   // Fabric master interface
+   interface AXI4_Master #( Wd_MId, Wd_Addr, Wd_Data
+                          , Wd_AW_User, Wd_W_User, Wd_B_User
+                          , Wd_AR_User, Wd_R_User) mem_master;
+
+`ifdef PERFORMANCE_MONITORING
+   method EventsL1I events;
+`endif
+
+   // ----------------------------------------------------------------
+   // Misc. control and status
+
+   // ----------------
+   // For ISA tests: watch memory writes to <tohost> addr
+
+`ifdef WATCH_TOHOST
+   method Action set_watch_tohost (Bool watch_tohost, Bit #(64) tohost_addr);
+   method Bit #(64) mv_tohost_value;
+`endif
+
+   // Inform core that DDR4 has been initialized and is ready to accept requests
+   method Action ma_ddr4_ready;
+
+   // Misc. status; 0 = running, no error
+   (* always_ready *)
+   method Bit #(8) mv_status;
+
+endinterface
+
+
+
+//typedef MMU_Cache_IFC#(Wd_MId_2x3) MMU_DCache_IFC;
+//typedef MMU_Cache_IFC#(Wd_MId) MMU_ICache_IFC;
 
 // ****************************************************************
 // ****************************************************************
@@ -468,15 +614,85 @@ endfunction
 // The module implementation
 
 (* synthesize *)
-module mkMMU_ICache(MMU_ICache_IFC);
-  let cache <- mkMMU_Cache(False, fabric_default_mid);
-  return cache;
+module mkMMU_DCache(MMU_DCache_IFC);
+  MMU_Cache_IFC#(Wd_MId_2x3) cache <- mkMMU_Cache(False, fabric_2x3_default_mid);
+  EventsL1D evts = EventsL1D { evt_LD           : zeroExtend(pack(cache.events.evt_LD))
+                             , evt_LD_MISS      : zeroExtend(pack(cache.events.evt_LD_MISS))
+                             , evt_LD_MISS_LAT  : zeroExtend(pack(cache.events.evt_LD_MISS_LAT))
+                             , evt_ST           : zeroExtend(pack(cache.events.evt_ST))
+                             , evt_ST_MISS      : zeroExtend(pack(cache.events.evt_ST_MISS))
+                             , evt_ST_MISS_LAT  : zeroExtend(pack(cache.events.evt_ST_MISS_LAT))
+                             , evt_AMO          : zeroExtend(pack(cache.events.evt_AMO))
+                             , evt_AMO_MISS     : zeroExtend(pack(cache.events.evt_AMO_MISS))
+                             , evt_AMO_MISS_LAT : zeroExtend(pack(cache.events.evt_AMO_MISS_LAT))
+                             , evt_TLB          : zeroExtend(pack(cache.events.evt_TLB))
+                             , evt_TLB_MISS     : zeroExtend(pack(cache.events.evt_TLB_MISS))
+                             , evt_TLB_MISS_LAT : zeroExtend(pack(cache.events.evt_TLB_MISS_LAT))
+                             , evt_TLB_FLUSH    : zeroExtend(pack(cache.events.evt_TLB_FLUSH))
+                             , evt_EVICT        : zeroExtend(pack(cache.events.evt_EVICT))
+                             };
+  method set_verbosity = cache.set_verbosity;
+  interface server_reset = cache.server_reset;
+  method req = cache.req;
+`ifdef ISA_CHERI
+  method commit = cache.commit;
+`endif
+  method valid = cache.valid;
+  method addr = cache.addr;
+  method cword = cache.cword;
+  method st_amo_val = cache.st_amo_val;
+  method exc = cache.exc;
+  method exc_code = cache.exc_code;
+  interface server_flush = cache.server_flush;
+  method tlb_flush = cache.tlb_flush;
+  interface mem_master = cache.mem_master;
+`ifdef WATCH_HOST
+  method set_watch_tohost = cache.set_watch_tohost;
+  method mv_tohost_value = cache.mv_tohost_value;
+`endif
+  method ma_ddr4_ready = cache.ma_ddr4_ready;
+  method mv_status = cache.mv_status;
+  method events = evts;
 endmodule
 (* synthesize *)
-module mkMMU_DCache(MMU_DCache_IFC);
-  let cache <- mkMMU_Cache(True, fabric_2x3_default_mid);
-  return cache;
+module mkMMU_ICache(MMU_ICache_IFC);
+  MMU_Cache_IFC#(Wd_MId) cache <- mkMMU_Cache(True, fabric_default_mid);
+  EventsL1I evts = EventsL1I { evt_LD           : zeroExtend(pack(cache.events.evt_LD))
+                             , evt_LD_MISS      : zeroExtend(pack(cache.events.evt_LD_MISS))
+                             , evt_LD_MISS_LAT  : zeroExtend(pack(cache.events.evt_LD_MISS_LAT))
+                             , evt_TLB          : zeroExtend(pack(cache.events.evt_TLB))
+                             , evt_TLB_MISS     : zeroExtend(pack(cache.events.evt_TLB_MISS))
+                             , evt_TLB_MISS_LAT : zeroExtend(pack(cache.events.evt_TLB_MISS_LAT))
+                             , evt_TLB_FLUSH    : zeroExtend(pack(cache.events.evt_TLB_FLUSH))
+                             };
+  method set_verbosity = cache.set_verbosity;
+  interface server_reset = cache.server_reset;
+  method req = cache.req;
+`ifdef ISA_CHERI
+  method commit = cache.commit;
+`endif
+  method valid = cache.valid;
+  method addr = cache.addr;
+  method cword = cache.cword;
+  method st_amo_val = cache.st_amo_val;
+  method exc = cache.exc;
+  method exc_code = cache.exc_code;
+  interface server_flush = cache.server_flush;
+  method tlb_flush = cache.tlb_flush;
+  interface mem_master = cache.mem_master;
+`ifdef WATCH_HOST
+  method set_watch_tohost = cache.set_watch_tohost;
+  method mv_tohost_value = cache.mv_tohost_value;
+`endif
+  method ma_ddr4_ready = cache.ma_ddr4_ready;
+  method mv_status = cache.mv_status;
+  method events = evts;
 endmodule
+//(* synthesize *)
+//module mkMMU_DCache(MMU_DCache_IFC);
+//  let cache <- mkMMU_Cache(True, fabric_2x3_default_mid);
+//  return cache;
+//endmodule
 module mkMMU_Cache  #(parameter Bool dmem_not_imem,
                       parameter Bit#(mID) default_mid)  (MMU_Cache_IFC#(mID));
 
@@ -2199,7 +2415,7 @@ module mkMMU_Cache  #(parameter Bool dmem_not_imem,
    interface mem_master = masterPortShim.master;
 
 `ifdef PERFORMANCE_MONITORING
-   interface EventsCache events = aw_events [0];
+   method EventsCache events = aw_events [0];
 `endif
 
 endmodule: mkMMU_Cache

src_Core/CPU/CPU.bsv
@@ -55,6 +55,7 @@ import PerformanceMonitor :: *;
 import Vector             :: *;
 import SpecialRegs        :: *;
 import StatCounters       :: *;
+import GenerateHPMVector  :: *;
 `endif
 
 // ================================================================
@@ -1137,17 +1138,28 @@ module mkCPU (CPU_IFC);
    // ================================================================
    // Performance counters
 
-   //Vector #(1, Bit #(Report_Width)) null_evt = replicate (0);
    //Vector #(31, Bit #(Report_Width)) core_evts_vec = to_large_vector (aw_events [0]);
+   EventsCore core_evts = aw_events [0];
    //Vector #(16, Bit #(Report_Width)) imem_evts_vec = to_large_vector (near_mem.imem.events);
+   EventsL1I imem_evts = near_mem.imem.events;
+   EventsL1D dmem_evts = near_mem.dmem.events;
    //Vector #(16, Bit #(Report_Width)) dmem_evts_vec = to_large_vector (near_mem.dmem.events);
    //Vector #(32, Bit #(Report_Width)) external_evts_vec = to_large_vector (crg_external_evts [0]);
+   AXI4_Slave_Events slave_evts = crg_slave_evts [0];
+   AXI4_Master_Events master_evts = crg_master_evts [0];
+   EventsCacheCore tag_cache_evts = crg_tag_cache_evts [0];
+
+   let ev_struct = HPMEvents {mab_EventsCore: tagged Valid core_evts, mab_EventsL1I: tagged Valid imem_evts,
+                              mab_EventsL1D: tagged Valid dmem_evts, mab_EventsLL: tagged Invalid,
+                              mab_EventsCacheCore: tagged Valid tag_cache_evts, mab_EventsTransExe: tagged Invalid,
+                              mab_AXI4_Slave_Events: tagged Valid slave_evts, mab_AXI4_Master_Events: tagged Valid master_evts};
 
    //let events = append (null_evt, core_evts_vec);
    //events = append (events, imem_evts_vec);
    //events = append (events, dmem_evts_vec);
    //events = append (events, external_evts_vec);
-   Vector#(No_Of_Evts, Bit#(Report_Width)) events = replicate(0);
+   //Vector#(No_Of_Evts, Bit#(Report_Width)) events = replicate(0);
+   let events = generateHPMVector(ev_struct);
 
    (* fire_when_enabled, no_implicit_conditions *)
    rule rl_send_perf_evts;

src_Core/Near_Mem_VM/Near_Mem_Caches.bsv
@@ -60,6 +60,10 @@ import MMU_Cache_Arbiter           :: *;
 import AXI4_Lite_MMU_Cache_Adapter :: *;
 `endif
 
+`ifdef PERFORMANCE_MONITORING
+import StatCounters :: *;
+`endif
+
 // System address map and pc_reset value
 import SoC_Map :: *;
 
@@ -190,7 +194,7 @@ module mkNear_Mem (Near_Mem_IFC);
       method WordXL   tval           = icache.addr;
 
 `ifdef PERFORMANCE_MONITORING
-      method EventsCache events = icache.events;
+      method EventsL1I events = unpack(0);
 `endif
    endinterface
 
@@ -240,7 +244,7 @@ module mkNear_Mem (Near_Mem_IFC);
       method Exc_Code   exc_code   = dcache.exc_code;
 
 `ifdef PERFORMANCE_MONITORING
-      method EventsCache events = dcache.events;
+      method EventsL1D events = unpack(0);
 `endif
    endinterface
 

src_Core/Near_Mem_VM/Near_Mem_IFC.bsv
@@ -53,6 +53,7 @@ import Fabric_Defs :: *;
 
 `ifdef PERFORMANCE_MONITORING
 import PerformanceMonitor :: *;
+import StatCounters :: *;
 `endif
 
 `ifdef INCLUDE_DMEM_SLAVE
@@ -79,9 +80,9 @@ typedef struct {
    Bool evt_EVICT;
 } EventsCache deriving (Bits, FShow);
 
-instance BitVectorable #(EventsCache, 1, m) provisos (Bits #(EventsCache, m));
-      function to_vector = struct_to_vector;
-endinstance
+//instance BitVectorable #(EventsCache, 1, m) provisos (Bits #(EventsCache, m));
+//      function to_vector = struct_to_vector;
+//endinstance
 `endif
 
 interface Near_Mem_IFC;
@@ -184,7 +185,7 @@ interface IMem_IFC;
    (* always_ready *)  method WordXL   tval;        // can be different from PC
 
 `ifdef PERFORMANCE_MONITORING
-   method EventsCache events;
+   method EventsL1I events;
 `endif
 endinterface
 
@@ -221,7 +222,7 @@ interface DMem_IFC;
    (* always_ready *)  method Exc_Code   exc_code;
 
 `ifdef PERFORMANCE_MONITORING
-   method EventsCache events;
+   method EventsL1D events;
 `endif
 endinterface
 

commit c49862c511cd6a5ac9dedb923b2b3133f53b3124

MMU_Cache.bsv: Fix inverted dmem_not_imem
These got inverted when refactoring, and broke all the ISA tests when
built without C, as the commit method is not called, and so the I$
response is squashed to a trap for the very first instruction. When C is
enabled mkCPU_Fetch_C wraps the I$ and has its own rule to always call
commit.

How this didn't break various other tests when C is enabled isn't clear;
it does things like force is_AMO* to False, and yet all the RVA tests
passed.

Fixes: e5c58a0a14e5 ("changed WT caches such that they are compatible with the new consistency approach")

src_Core/Near_Mem_VM/MMU_Cache.bsv
@@ -551,7 +551,7 @@ endfunction
 
 (* synthesize *)
 module mkMMU_DCache(MMU_DCache_IFC);
-  MMU_Cache_IFC#(Wd_MId_2x3) cache <- mkMMU_Cache(False, fabric_2x3_default_mid);
+  MMU_Cache_IFC#(Wd_MId_2x3) cache <- mkMMU_Cache(True, fabric_2x3_default_mid);
   EventsL1D evts = EventsL1D { evt_LD           : zeroExtend(pack(cache.events.evt_LD))
                              , evt_LD_MISS      : zeroExtend(pack(cache.events.evt_LD_MISS))
                              , evt_LD_MISS_LAT  : zeroExtend(pack(cache.events.evt_LD_MISS_LAT))
@@ -592,7 +592,7 @@ module mkMMU_DCache(MMU_DCache_IFC);
 endmodule
 (* synthesize *)
 module mkMMU_ICache(MMU_ICache_IFC);
-  MMU_Cache_IFC#(Wd_MId) cache <- mkMMU_Cache(True, fabric_default_mid);
+  MMU_Cache_IFC#(Wd_MId) cache <- mkMMU_Cache(False, fabric_default_mid);
   EventsL1I evts = EventsL1I { evt_LD           : zeroExtend(pack(cache.events.evt_LD))
                              , evt_LD_MISS      : zeroExtend(pack(cache.events.evt_LD_MISS))
                              , evt_LD_MISS_LAT  : zeroExtend(pack(cache.events.evt_LD_MISS_LAT))